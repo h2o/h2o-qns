3d29a50d2868ac62bb5ac4b0bc31848ce135645b
Jana Iyengar <jri.ietf@gmail.com>
2019-08-29 16:32:02 -0700

    Add logs

--- run_endpoint.sh
+++ run_endpoint.sh
@@ -9,9 +9,9 @@ shift
 cd quicly
 
 if [ "$ROLE" == "client" ]; then
-    echo "Starting h2o client ..."
-    ./cli -p /100000000.txt server 4434
+    echo "Starting quicly client ..."
+    ./cli -p /10000000.txt -e /qnslogs/quicly-cli.out server 4434
 elif [ "$ROLE" == "server" ]; then
-    echo "Starting h2o server ..."
-    ./cli -k server.key -c server.crt -e srv.out 0.0.0.0 4434
+    echo "Starting quicly server ..."
+    ./cli -k server.key -c server.crt -e /qnslogs/quicly-srv.out 0.0.0.0 4434
 fi
