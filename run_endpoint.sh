290025d02fcac45be24a559df7071b439872c745
Jana Iyengar <jri.ietf@gmail.com>
2019-09-20 19:10:12 -0700

    Make quicly work with interop scripts

--- run_endpoint.sh
+++ run_endpoint.sh
@@ -2,16 +2,40 @@
 
 # Set up the routing needed for the simulation.
 /setup.sh
-
 cd quicly
+
+if [ ! -z "$TESTCASE" ]; then
+    case "$TESTCASE" in
+        "transfer"|"retry") ;;
+        "resumption") exit 127 ;;
+        *) exit 127 ;;
+    esac
+fi
 
 if [ "$ROLE" == "client" ]; then
     sleep 10
+    cd /downloads
+    case "$TESTCASE" in
+        *) ;;
+    esac
     echo "Starting quicly client ..."
-    echo "CLIENT_PARAMS:" $CLIENT_PARAMS
-    ./cli $CLIENT_PARAMS -e /qnslogs/quicly-cli.out server 4434
+    if [ ! -z "$REQUESTS" ]; then
+        for REQ in $REQUESTS; do
+            FILE=`echo $REQ | cut -f4 -d'/'`
+            echo "/quicly/cli -p /$FILE -e /logs/$TESTCASE-$FILE.out server 443"
+            /quicly/cli -p /$FILE -e /logs/$TESTCASE-$FILE.out server 4434 > $FILE
+        done
+    fi
+
 elif [ "$ROLE" == "server" ]; then
+    echo "Starting server for test:" $TESTCASE
+    cd /www && ls -l
+    case "$TESTCASE" in
+        "retry") TEST_PARAMS="-R" ;;
+        *) ;;
+    esac
     echo "Starting quicly server ..."
-    echo "SERVER_PARAMS:" $SERVER_PARAMS
-    ./cli $SERVER_PARAMS -e /qnslogs/quicly-srv.out -k server.key -c server.crt 0.0.0.0 4434
+    echo "SERVER_PARAMS:" $SERVER_PARAMS "TEST_PARAMS:" $TEST_PARAMS
+    echo "/quicly/cli $SERVER_PARAMS $TEST_PARAMS -k /quicly/server.key -c /quicly/server.crt -e /logs/$TESTCASE.out 0.0.0.0 443"
+    /quicly/cli $SERVER_PARAMS $TEST_PARAMS -k /quicly/server.key -c /quicly/server.crt  -e /logs/$TESTCASE.out 0.0.0.0 4434
 fi
